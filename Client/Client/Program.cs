f3db7a451c001df6713b94072aec928781d5b371
unknown <honc.malgorzata@gmail.com>
2024-01-02 18:25:53 +0100

    Creating microservice for three instances in main.py and Client.cs app

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -6,24 +6,41 @@ class Program
 {
     static async Task Main(string[] args)
     {
+        // Lista adresów URL instancji mikroserwisu
+        string[] serviceUrls = {
+            "http://localhost:5000/add",
+            "http://localhost:5000/subtract",
+            "http://localhost:5000/multiply",
+            "http://localhost:5000/divide",
+            "http://localhost:5001/add",
+            "http://localhost:5001/subtract",
+            "http://localhost:5001/multiply",
+            "http://localhost:5001/divide",
+            "http://localhost:5002/add",
+            "http://localhost:5002/subtract",
+            "http://localhost:5002/multiply",
+            "http://localhost:5002/divide"
+        };
+
+        int currentInstanceIndex = 0;
+
+
         Console.WriteLine("Wybierz typ działania (add, subtract, multiply, divide):"); 
         String operation = Console.ReadLine();
 
         Console.WriteLine("Typ działania to: " + operation);
 
-        Console.ReadKey();
 
-         Console.WriteLine("Podaj pierwszą liczbę: ");
+        Console.WriteLine("Podaj pierwszą liczbę: ");
         decimal num1 = Convert.ToDecimal(Console.ReadLine());
 
         Console.WriteLine("Podaj drugą liczbę: ");
         decimal num2 = Convert.ToDecimal(Console.ReadLine()); 
 
         
-
         using (HttpClient client = new HttpClient())
         {
-            string apiUrl = $"http://localhost:5000/{operation}"; // Adres URL mikroserwisu
+             string apiUrl = serviceUrls[currentInstanceIndex]; // Wybieranie adresu URL z listy
 
             var requestData = new
             {
@@ -44,6 +61,8 @@ class Program
                 Console.WriteLine("Błąd w żądaniu HTTP.");
             }
         } 
+        
+        currentInstanceIndex = (currentInstanceIndex + 1) % serviceUrls.Length; // Przejście do następnej instancji
         
     }  
 }
